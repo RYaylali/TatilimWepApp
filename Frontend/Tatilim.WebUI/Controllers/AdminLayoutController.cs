d869be898ff626c4c5a82653b1812a1ab5dd3e06
Ramazan YAYLALI <ryaylali97@gmail.com>
2023-04-04 02:59:11 +0300

    partial viewler oluşturuldu

---
+++ Frontend/Tatilim.WebUI/Controllers/AdminLayoutController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tatilim.WebUI.Controllers
+{
+    public class AdminLayoutController : Controller
+    {
+        public IActionResult _AdminLayout()
+        {
+            return View();
+        }
+        public PartialViewResult NavPartial()
+        {
+            return PartialView();
+        }
+		public PartialViewResult PreloaderPartial()
+		{
+			return PartialView();
+		}
+		public PartialViewResult HeaderPartial()
+		{
+			return PartialView();
+		}
+		public PartialViewResult SidebarPartial()
+		{
+			return PartialView();
+		}
+		public PartialViewResult FooterPartial()
+		{
+			return PartialView();
+		}
+		public PartialViewResult ScriptPartial()
+		{
+			return PartialView();
+		}
+	}
+}
--- Frontend/Tatilim.WebUI/Program.cs
+++ Frontend/Tatilim.WebUI/Program.cs
@@ -6,10 +6,10 @@ namespace Tatilim.WebUI
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-            builder.Services.AddControllersWithViews();
+			// Add services to the container.
+			builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 
-            var app = builder.Build();
+			var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
