68933fc0cda010851c05e544a0ee594c31284785
Ramazan YAYLALI <ryaylali97@gmail.com>
2023-05-09 20:48:01 +0300

    RapidApi1

---
+++ RapidApi/RapidApiConsume/Controllers/ExchangeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RapidApiConsume.Models;
+
+namespace RapidApiConsume.Controllers
+{
+	public class ExchangeController : Controller
+	{
+		public async Task<IActionResult> Index()
+		{
+			
+			var client = new HttpClient();
+			var request = new HttpRequestMessage
+			{
+				Method = HttpMethod.Get,
+				RequestUri = new Uri("https://booking-com.p.rapidapi.com/v1/metadata/exchange-rates?currency=TRY&locale=en-gb"),
+				Headers =
+	{
+		{ "X-RapidAPI-Key", "6d6022c3b4mshbc36fc3400f0258p181912jsn103af4efdabe" },
+		{ "X-RapidAPI-Host", "booking-com.p.rapidapi.com" },
+	},
+			};
+			using (var response = await client.SendAsync(request))
+			{
+				response.EnsureSuccessStatusCode();
+				var body = await response.Content.ReadAsStringAsync();
+				var values =JsonConvert.DeserializeObject<ExchangeVM>(body);
+				return View(values.exchange_rates.ToList());
+			}
+			
+		}
+	}
+}
---
+++ RapidApi/RapidApiConsume/Controllers/SearchLocationIDController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RapidApiConsume.Models;
+
+namespace RapidApiConsume.Controllers
+{
+    public class SearchLocationIDController : Controller
+    {
+        public async Task<IActionResult> Index(string cityName)
+        {
+            if (!string.IsNullOrEmpty(cityName))
+            {
+                List<BookingApiLocationSearchVM> model = new List<BookingApiLocationSearchVM>();
+                var client = new HttpClient();
+                var request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Get,
+                    RequestUri = new Uri($"https://booking-com.p.rapidapi.com/v1/hotels/locations?name={cityName}&locale=en-gb"),
+                    Headers =
+    {
+        { "X-RapidAPI-Key", "6d6022c3b4mshbc36fc3400f0258p181912jsn103af4efdabe" },
+        { "X-RapidAPI-Host", "booking-com.p.rapidapi.com" },
+    },
+                };
+                using (var response = await client.SendAsync(request))
+                {
+                    response.EnsureSuccessStatusCode();
+                    var body = await response.Content.ReadAsStringAsync();
+                    model = JsonConvert.DeserializeObject<List<BookingApiLocationSearchVM>>(body);
+                    return View(model.Take(1).ToList());//take ile indexin 1 incisini almaya yarar
+                }
+            }
+            else
+            {
+                List<BookingApiLocationSearchVM> model = new List<BookingApiLocationSearchVM>();
+                var client = new HttpClient();
+                var request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Get,
+                    RequestUri = new Uri("https://booking-com.p.rapidapi.com/v1/hotels/locations?name=Berlin&locale=en-gb"),
+                    Headers =
+    {
+        { "X-RapidAPI-Key", "6d6022c3b4mshbc36fc3400f0258p181912jsn103af4efdabe" },
+        { "X-RapidAPI-Host", "booking-com.p.rapidapi.com" },
+    },
+                };
+                using (var response = await client.SendAsync(request))
+                {
+                    response.EnsureSuccessStatusCode();
+                    var body = await response.Content.ReadAsStringAsync();
+                    model = JsonConvert.DeserializeObject<List<BookingApiLocationSearchVM>>(body);
+                    return View(model.Take(1).ToList());//take ile indexin 1 incisini almaya yarar
+                }
+            }
+
+        }
+    }
+}
